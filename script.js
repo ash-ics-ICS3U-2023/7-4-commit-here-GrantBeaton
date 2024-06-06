27e21478f1bf1294ae492f3d026de1df1c6cdde4
Grant Beaton <grant.beaton@stu.ocsb.ca>
2024-06-06 13:42:41 -0400

    Moved stuff over

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 /**
  * This file will get loaded by index.html
  */
+console.log("Hello World!");
+console.log(document.title);
+
+let clicks = 0;
+document.getElementById("add_click_btn").addEventListener("click", addclicks);
+
+function addclicks() {
+    clicks++;
+    document.getElementById("clicks").textContent ++ ;
+    console.log("The button was clicked!");
+}
+
+document.getElementById("sub_click_btn").addEventListener("click", subclicks);
+
+function subclicks() {
+    clicks--;
+    document.getElementById("clicks").textContent -- ;
+    console.log("The button was clicked!");
+}
+
+document.getElementById("reset_click_btn").addEventListener("click", resetclicks);
+
+function resetclicks() {
+    clicks = 0;
+    document.getElementById("clicks").textContent = 0 ;
+    console.log("The button was clicked!");
+}
